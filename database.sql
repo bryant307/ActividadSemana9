5d7eaa7f3db105af2c5ae738c5162b57083eb85f
bryant307 <torresb307@gmail.com>
2025-03-18 14:46:29 -0600

    final

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE empleados;
-USE empleados;
-
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    correo VARCHAR(100) NOT NULL,
-    edad INT NOT NULL
-);

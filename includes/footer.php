703fd82376ee21d0c95addbce64caa911529d919
bryant307 <torresb307@gmail.com>
2025-03-18 14:00:41 -0600

    mejoras css

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,11 @@
+
+<link rel="stylesheet" href="../css/style.css">
 <!-- footer.php -->
-<footer>
-        <p>CRUD PHP con MariaDB - <?php echo date("Y"); ?></p>
+<footer class="footer">
+        <p class="footer__letras">CRUD PHP con MariaDB - <?php echo date("Y"); ?></p>
+        <p class="footer__letras">Nombre de los Estudiantes: Bryan Enrique Torres Alvarez</p>
+        <p class="footer__letras" >Flavid Elias Reyes Paz</p>
+        <p class="footer__letras">Laboratorio 1 - Segundo Computo</p>
     </footer>
 </body>
 </html>
